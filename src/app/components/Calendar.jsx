4b2063ad43515d448a4277fcb62304fe3a52a09a
Maria <cherryalmondtea@gmail.com>
2025-03-24 11:25:31 +0000

    calendar setup

---
+++ src/app/components/Calendar.jsx
@@ -0,0 +1,44 @@
+'use client';
+
+import React from 'react';
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import { format, parse, startOfWeek, getDay } from 'date-fns';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { enUS } from 'date-fns/locale';
+
+const locales = {
+  'en-US': enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const MyCalendar = () => {
+  const events = [
+    {
+      title: 'Sample Event',
+      start: new Date(),
+      end: new Date(),
+    },
+  ];
+
+  return (
+    <div style={{ height: 500 }}>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: '100%' }}
+      />
+    </div>
+  );
+};
+
+export default MyCalendar;
---
+++ src/app/page1/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calendar from '../components/Calendar';
+
+const Page1 = () => {
+  return (
+    <div>
+      <h1>My calendar</h1>
+      <Calendar />
+    </div>
+  );
+};
+
+export default Page1;
