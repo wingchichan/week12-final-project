bfb0a6d74a7c0a169c7f3ab637f06998189af89a
Taran Sahota <tazsahota2003@gmail.com>
2025-03-25 14:22:56 +0000

    added dynamic routes

---
+++ src/app/calendars/[id]/page.js
@@ -0,0 +1,14 @@
+import {auth} from "@clerk/nextjs/server"
+import {db} from "@/utilities/connect"
+import { redirect } from "next/navigation"
+
+export default async function UserCalendar( {params} ) {
+    const { id } = await params; //Gets the calendars.id from params.
+
+    const calendarData = await db.query(`SELECT * FROM calendars WHERE id = $1`,[ id ])
+    const individualCalendar = calendarData.rows[0]
+
+    return (
+        <p>{individualCalendar.name}</p>
+    )
+}
