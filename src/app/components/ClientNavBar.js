e32df06dbd1964d41e1e7aca0bb56f2620f901dd
Maria <cherryalmondtea@gmail.com>
2025-03-26 13:54:33 +0000

    search-bar

--- src/app/components/ClientNavBar.js
+++ src/app/components/ClientNavBar.js
@@ -1,4 +1,3 @@
-// components/ClientNavBar.js
 'use client';
 import { usePathname } from 'next/navigation';
 import NavBar from './NavBar';
@@ -6,8 +5,7 @@ import NavBar from './NavBar';
 const ClientNavBar = () => {
   const pathname = usePathname();
 
-  // Если текущий путь не главная страница ('/'), то отображаем NavBar
-  if (pathname === '/') return null; // На главной странице не показываем навбар
+  if (pathname === '/') return null;
 
   return <NavBar />;
 };
---
+++ src/app/components/SearchInput.js
@@ -0,0 +1,38 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+export const SearchInput = () => {
+  const router = useRouter();
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSearch = () => {
+    if (inputValue) {
+      router.push(`/all-calendars?q=${inputValue}`);
+    }
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
+  return (
+    <div className="search__input">
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleChange}
+        onKeyDown={handleKeyPress}
+        placeholder="search calendars"
+      />
+    </div>
+  );
+};
+
