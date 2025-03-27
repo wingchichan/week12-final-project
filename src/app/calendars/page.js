b4c8412365a4c1d370c41eeb639c1d39a0154f43
Taran Sahota <tazsahota2003@gmail.com>
2025-03-27 13:14:48 +0000

    added it

--- src/app/calendars/page.js
+++ src/app/calendars/page.js
@@ -5,12 +5,16 @@ import "./page.css";
 import Link from "next/link";
 import { useUser } from "@clerk/nextjs";
 import { getName } from "@/utilities/actions";
-
+//import connect
 // import NameCalendarForm from '../components/NameCalendarForm'
 
 const MyCalendar = () => {
-  // const { user } = useUser();
+  // const { user } = useUser(); REPLACE WITH AUTH
   // const userName = user?.firstName || "Guest";
+
+  //db.query get events info based on user id, pass an event list to the calendar
+  //ask chatGPT, give it info from databse, ask it how to format it to fit the props of the calendar component
+  
 
   return (
     <div>
